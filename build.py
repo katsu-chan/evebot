7e4335ad04b022e5a6503837b0e27330c8d83d8f
katsu-chan <44003480+katsu-chan@users.noreply.github.com>
2022-06-21 22:02:45 +0300

    ive been denied all the best ultrasex

--- build.py
+++ build.py
@@ -5,18 +5,14 @@ const token = ""
 '''
 new = 'const token = "' + sys.argv[1] +'"'
 
-f = open("build/functions/api/index.ts", "rw")
-
+f = open("build/functions/api/index.ts", "r")
 src = f.read()
+f.close()
 
 print(src)
 
 new2 = src.replace(old, new)
 
+f = open("build/functions/api/index.ts", "r")
 f.write(new2)
-
-new3 = f.read()
-
-print(new3)
-
 f.close()
